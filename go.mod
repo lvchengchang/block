3d596535be17be89ceec63eddec8aa07385ae4dc
lvchengxu <lvchengxu@kuaidihelp.com>
2019-05-28 17:52:51 +0800

    1) 增加钱包地址获取

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module cblock
 
 go 1.12
 
-require github.com/boltdb/bolt v1.3.1
+require (
+	github.com/boltdb/bolt v1.3.1
+	github.com/btcsuite/btcutil v0.0.0-20190425235716-9e5f4b9a998d
+	golang.org/x/crypto v0.0.0-20190513172903-22d7a77e9e5f
+)
---
+++ wallet.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"github.com/btcsuite/btcutil/base58"
+	"golang.org/x/crypto/ripemd160"
+	"log"
+)
+
+type Wallet struct {
+	Private *ecdsa.PrivateKey
+	Public  []byte
+}
+
+// 创建钱包
+func NewWallet() *Wallet {
+	curve := elliptic.P256()
+	privateKey, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	public := privateKey.PublicKey
+	// 拼接横纵坐标
+	pubkey := append(public.X.Bytes(), public.Y.Bytes()...)
+
+	return &Wallet{
+		Public:  pubkey,
+		Private: privateKey,
+	}
+}
+
+func (w *Wallet) NewAddress() string {
+	public := w.Public
+
+	value := HashPubKey(public)
+	version := byte(00)
+	payload := append([]byte{version}, value...)
+
+	checkCode := CheckSum(payload)
+	payload = append(payload, checkCode...)
+
+	// golang btcd 比特币全节点
+	address := base58.Encode(payload)
+	return address
+}
+
+func HashPubKey(data []byte) []byte {
+	hash := sha256.Sum256(data)
+
+	rip160hasher := ripemd160.New()
+	_, err := rip160hasher.Write(hash[:])
+	if err != nil {
+		log.Println(err)
+	}
+
+	// 返回rip160的哈希结果
+	return rip160hasher.Sum(nil)
+}
+
+func CheckSum(data []byte) []byte {
+	// checksum
+	hash1 := sha256.Sum256(data)
+	hash2 := sha256.Sum256(hash1[:])
+
+	return hash2[:4]
+}
